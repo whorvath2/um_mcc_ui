28c3819d28a6bff60d6d3ba5e63560b58cea5131
Bill Horvath <bhorvath@umich.edu>
2023-02-04 19:58:17 -0500

    Cost calculation now works. We have an MVP.

--- src/search.js
+++ src/search.js
@@ -2,10 +2,12 @@ import React from 'react';
 import StafferList from "./stafferList";
 
 export default function Search() {
+    const [minutesState, setMinutesState] = React.useState(0)
     const [termState, setTermState] = React.useState("");
+    const [costState, setCostState] = React.useState("$0");
     const [resultsState, _setResultsState] = React.useState([]);
     const [chosenState, _setChosenState] = React.useState([]);
-    
+
     const resultsStateRef = React.useRef(resultsState)
     const chosenStateRef = React.useRef(chosenState)
     const setResultsState = data => {
@@ -36,16 +38,34 @@ export default function Search() {
             return [];
         }
         fetch('http://localhost:8662/um_mcc/find?' + query)
-            .then(response => {
-                return response.json();
-            }).then(data => {
+            .then(response => response.json())
+            .then(data => {
                 data.forEach(item => {item.chosen = false})
                 setResultsState(data ? data : []);
             })
-            .catch((error) => {
-                return console.log(error);
-            });
+            .catch((error) => console.log(error));
     }
+
+    function calculateCost(e){
+        e.preventDefault();
+        if (!minutesState){
+            return 0
+        }
+        fetch('http://localhost:8662/um_mcc/cost/' + minutesState, {
+            method: 'POST',
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(chosenState)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setCostState("$" + data.cost);
+            })
+            .catch(error => console.log(error));
+    }
+
     return (
         <div className="Search">
             <h5 className="title">Find Staff</h5>
@@ -68,6 +88,17 @@ export default function Search() {
                     <div className="col">
                         <label className="col-form-label">Selected Attendees:</label>
                         <StafferList stateRef={chosenStateRef} makeChoice={makeChoice} areChosen={true}/>
+                        <form onSubmit={calculateCost}>
+                            <label className="label left">Meeting Length:
+                                <input id="meetingLength"
+                                       className="form-control"
+                                       type="number"
+                                       placeholder="Enter length in minutes"
+                                       onChange={event => setMinutesState(event.target.valueAsNumber)}/>
+                            </label>
+                            <button className={"btn btn-primary"}>Calculate Meeting Cost</button>
+                        </form>
+                        <p>Meeting Cost: {costState}</p>
                     </div>
                 </div>
             </div>
